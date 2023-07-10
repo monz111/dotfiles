2d6bb3625a05bc11e1681e5ff519221cad2ebaa3
yuya.mozen <yuya.monzen@edge-inc.co.jp>
2023-07-11 02:21:03 +0900

    chore: update lsp config & gitsigns

--- nvchad/custom/configs/overrides.lua
+++ nvchad/custom/configs/overrides.lua
@@ -2,7 +2,39 @@ local M = {}
 
 M.mason = {
   ensure_installed = {
+    "lua-language-server",
+    "stylua",
     "rust-analyzer",
+    "css-lsp",
+    "html-lsp",
+    "prettier",
+    "typescript-language-server",
+    "deno",
+    "emmet-ls",
+    "json-lsp",
+    "shfmt",
+    "shellcheck",
+    "intelephense",
+  },
+}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "rust",
+    "html",
+    "css",
+    "scss",
+    "sql",
+    "markdown",
+    "javascript",
+    "typescript",
+    "tsx",
+    "json",
+    "c",
+    "yaml",
+    "zig"
   },
 }
 
@@ -19,17 +51,29 @@ M.nvimtree = {
       },
       glyphs = {
         git = {
-          unstaged = "",
-          staged = "✓",
+          unstaged = "",
+          staged = "",
           unmerged = "",
-          renamed = "➜",
-          untracked = "★",
+          renamed = "",
+          untracked = "",
           deleted = "",
-          ignored = "◌",
+          ignored = "",
         },
       },
     },
   },
 }
+
+M.gitsigns = {
+  signs = {
+    add = { text = "" },
+    change = { text = "" },
+    delete = { text = "󰍵" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "│" },
+  },
+}
+
 
 return M
