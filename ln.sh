fefa732a0ad49218aac30217570c87a72751b633
yuya.mozen <yuya.monzen@edge-inc.co.jp>
2024-04-07 04:28:29 +0900

    chore: delete nvchad config

--- ln.sh
+++ ln.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/dotfiles/nvchad/custom ~/.config/nvim/lua/
-
+ln -nfs ~/dotfiles/.config/nvim ~/.config/nvim
