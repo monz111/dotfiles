2f759450942a827ce616d49baae2796ac385e835
yuya.mozen <yuya.monzen@edge-inc.co.jp>
2024-04-11 20:57:57 +0900

    feat: update transparent colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,26 +3,31 @@ local M = {
   lazy = false,
   name = "catppuccin",
   priority = 1000,
+  transparent_background = true,
   integrations = {
     cmp = true,
     gitsigns = true,
     nvimtree = true,
     treesitter = true,
     notify = true,
+    mason = true,
     mini = {
       enabled = true,
       indentscope_color = "",
     },
-    fidget = false,
+    fidget = true,
     harpoon = false,
-    leap = false,
+    leap = true,
     markdown = true,
-  }
+    alpha = true,
+  },
 }
 
 function M.config()
-  vim.cmd.colorscheme "catppuccin"
+  local cat = require "catppuccin"
+  cat.options.transparent_background = true
+  cat.compile()
+  vim.cmd.colorscheme "catppuccin-mocha"
 end
 
 return M
-
