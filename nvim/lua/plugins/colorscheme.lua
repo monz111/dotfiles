4a162ee3ce5a6e1b00e1853eaa9b1abd44ff0c5f
yuya.mozen <yuya.monzen@edge-inc.co.jp>
2024-04-12 14:56:00 +0900

    chore: delete transparent plugin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -28,6 +28,14 @@ function M.config()
   cat.options.transparent_background = true
   cat.compile()
   vim.cmd.colorscheme "catppuccin-mocha"
+
+  local keymap = vim.keymap.set
+  keymap("n", "<leader>tb", function()
+    local cat = require "catppuccin"
+    cat.options.transparent_background = not cat.options.transparent_background
+    cat.compile()
+    vim.cmd.colorscheme(vim.g.colors_name)
+  end)
 end
 
 return M
