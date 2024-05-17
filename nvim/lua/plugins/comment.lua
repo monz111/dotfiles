c9fad07f2e9341f36352d908e452f6507b00f22c
yuya.mozen <yuya.monzen@edge-inc.co.jp>
2024-05-18 02:02:35 +0900

    chore: add mysql comment

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -28,6 +28,8 @@ function M.config()
   require("Comment").setup {
     pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
   }
+
+  require('Comment.ft').mysql = { '-- %s', '/* %s */' }
 end
 
 return M
