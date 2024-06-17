19d314150d53992a2b3a29088c3fbc1c162c8dee
yuya.mozen <yuya.monzen@edge-inc.co.jp>
2024-06-17 13:22:11 +0900

    chore: update oil.lua

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -12,12 +12,23 @@ function M.config()
   }
 
   require("oil").setup {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
     view_options = {
       show_hidden = true,
+      natural_order = true,
+      is_always_hidden = function(name, _)
+        return name == ".." or name == ".git"
+      end,
     },
     float = {
       max_height = 30,
       max_width = 60,
+    },
+    win_options = {
+      wrap = true,
+      winblend = 0,
     },
     keymaps = {
       ["<ESC>"] = "actions.close",
