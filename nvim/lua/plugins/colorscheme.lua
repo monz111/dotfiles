71c667d4d5669fa53061fe40080a0fef5a0a6d3c
monz <yuya.monzen@edge-inc.co.jp>
2024-10-06 16:14:50 +0900

    chore: updated colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,44 @@
 local M = {
   "catppuccin/nvim",
-  lazy = false,
   name = "catppuccin",
   priority = 1000,
-  transparent_background = true,
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    nvimtree = true,
-    treesitter = true,
-    notify = true,
-    mason = true,
-    mini = {
-      enabled = true,
-      indentscope_color = "",
-    },
-    fidget = true,
-    harpoon = false,
-    leap = true,
-    markdown = true,
-    alpha = true,
-  },
 }
 
 function M.config()
-  local cat = require "catppuccin"
-  cat.options.transparent_background = true
-  cat.compile()
+  require("catppuccin").setup {
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+      light = "latte",
+      dark = "mocha",
+    },
+    default_integrations = false,
+    show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+    term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_1`)
+    transparent_background = true, -- disables setting the background color.
+    integrations = {
+      alpha = true,
+      cmp = true,
+      fidget = true,
+      gitsigns = true,
+      harpoon = true,
+      hop = false,
+      markdown = true,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      neogit = true,
+      notify = true,
+      nvimtree = true,
+      telescope = {
+        enabled = true,
+      },
+      treesitter = true,
+      ufo = true,
+      which_key = true,
+    },
+  }
+
   vim.cmd.colorscheme "catppuccin"
 
   local keymap = vim.keymap.set
