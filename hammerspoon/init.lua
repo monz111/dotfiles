4ff05f31d1c7fc065e7bb501519677a7d1fb7c97
monz <yuya.monzen@edge-inc.co.jp>
2024-10-10 06:19:27 +0900

    feat: add window padding

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,14 +1,28 @@
 hs.window.animationDuration = 0
 
+local margin = 20
+local screen = hs.screen.primaryScreen()
+local screenFrame = screen:frame()
+
+local function applyMargin(rect, isMain)
+  local adjustedMargin = isMain and margin or (2 * margin)
+  return {
+    x = rect.x + margin / screenFrame.w,
+    y = rect.y + margin / screenFrame.h,
+    w = rect.w - adjustedMargin / screenFrame.w,
+    h = rect.h - (2 * margin) / screenFrame.h,
+  }
+end
+
 local layout = {
-  left30 = { x = 0, y = 0, w = 0.3, h = 1 },
-  right70 = { x = 0.3, y = 0, w = 0.7, h = 1 },
-  left70 = { x = 0, y = 0, w = 0.7, h = 1 },
-  right30 = { x = 0.7, y = 0, w = 0.3, h = 1 },
-  left40 = { x = 0, y = 0, w = 0.4, h = 1 },
-  right60 = { x = 0.4, y = 0, w = 0.6, h = 1 },
-  left60 = { x = 0, y = 0, w = 0.6, h = 1 },
-  right40 = { x = 0.6, y = 0, w = 0.4, h = 1 },
+  left30 = applyMargin({ x = 0, y = 0, w = 0.3, h = 1 }, true),
+  right70 = applyMargin({ x = 0.3, y = 0, w = 0.7, h = 1 }),
+  left70 = applyMargin({ x = 0, y = 0, w = 0.7, h = 1 }, true),
+  right30 = applyMargin({ x = 0.7, y = 0, w = 0.3, h = 1 }),
+  left40 = applyMargin({ x = 0, y = 0, w = 0.4, h = 1 }, true),
+  right60 = applyMargin({ x = 0.4, y = 0, w = 0.6, h = 1 }),
+  left60 = applyMargin({ x = 0, y = 0, w = 0.6, h = 1 }, true),
+  right40 = applyMargin({ x = 0.6, y = 0, w = 0.4, h = 1 }),
 }
 
 local watchedApps = { "Arc", "kitty", "Slack" }
