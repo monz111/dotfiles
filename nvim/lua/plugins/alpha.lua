f0da6199ec80272f23e9911f35bb3a36399e7c4a
monz <yuya.monzen@edge-inc.co.jp>
2025-03-05 11:12:56 +0900

    feat: add serple

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -26,7 +26,6 @@ function M.config()
     button("n", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
     button("p", icons.git.Repo .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
     button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-    button("s", icons.ui.History .. " Session List", ":lua require('auto-session.session-lens').search_session()<CR>"),
     button("t", icons.ui.Text .. " Find text", ":Telescope live_grep <CR>"),
     button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
     button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
