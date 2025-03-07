ee5ed09462faff297dc576342b9e0b2c76279ba0
monz111 <yuya.monzen@edge-inc.co.jp>
2025-03-07 19:42:33 +0900

    chore: updated smart-splits keymaps

--- nvim/lua/plugins/smart-splits.lua
+++ nvim/lua/plugins/smart-splits.lua
@@ -3,25 +3,40 @@ local M = {
 }
 
 function M.config()
+  local icons = require "icons"
   local wk = require "which-key"
   wk.add {
-    { "<leader>wr", "<cmd>lua require('smart-splits').start_resize_mode()<cr>", desc = "SmartSplits start resize mode" },
+    {
+      "<leader>wr",
+      "<cmd>lua require('smart-splits').start_resize_mode()<cr>",
+      desc = "[SmartSplits] Resize Mode",
+      icon = icons.ui.Resize,
+    },
+    {
+      "<leader>wh",
+      "<cmd>lua require('smart-splits').swap_buf_left()<cr>",
+      desc = "Swap Left",
+      icon = icons.ui.BoldArrowLeft,
+    },
+    {
+      "<leader>wj",
+      "<cmd>lua require('smart-splits').swap_buf_down()<cr>",
+      desc = "Swap Down↓",
+      icon = icons.ui.BoldArrowDown,
+    },
+    {
+      "<leader>wk",
+      "<cmd>lua require('smart-splits').swap_buf_up()<cr>",
+      desc = "Swap Up↑",
+      icon = icons.ui.BoldArrowUp,
+    },
+    {
+      "<leader>wl",
+      "<cmd>lua require('smart-splits').swap_buf_right()<cr>",
+      desc = "Swap Right",
+      icon = icons.ui.BoldArrowRight,
+    },
   }
-  vim.keymap.set("n", "<C-w>h", require("smart-splits").resize_left)
-  vim.keymap.set("n", "<C-w>j", require("smart-splits").resize_down)
-  vim.keymap.set("n", "<C-w>k", require("smart-splits").resize_up)
-  vim.keymap.set("n", "<C-w>l", require("smart-splits").resize_right)
-  -- moving between splits
-  vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-  vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-  vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-  vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-  vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
-  -- swapping buffers between windows
-  vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
-  vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
-  vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
-  vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
 end
 
 return M
