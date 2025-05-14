48917c29c588c338ecf34794818794715e3c9347
monz111 <yuya.monzen@edge-inc.co.jp>
2025-05-14 13:24:36 +0900

    chore(nvim): updated cursor thickness

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -29,6 +29,7 @@ vim.opt.spell = false
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.cursorlineopt = "number"
+vim.opt.guicursor = "i:ver25"
 vim.opt.number = true -- set numbered lines
 vim.opt.laststatus = 0
 vim.opt.showcmd = false
